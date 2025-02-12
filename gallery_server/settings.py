55a86685bd08ec208f707ee2bf67f8e9b4cd6279
marcosviana91 <marcos.viana.91@gmail.com>
2025-02-12 17:36:22 -0300

    refactor - DEBUG variable set moved to .env file fix - add 'localhost' to ALLOWED_HOSTS

--- gallery_server/settings.py
+++ gallery_server/settings.py
@@ -23,13 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['192.168.1.31','192.168.1.32', 'marcosvianadev.ddns.net']
+ALLOWED_HOSTS = ['localhost', '192.168.1.31','192.168.1.32', 'marcosvianadev.ddns.net']
 
 
 # Application definition
