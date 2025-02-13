f3eadfa3f0fece2a1c3d9926bf2f7660b3978be8
marcosviana91 <marcos.viana.91@gmail.com>
2025-02-12 23:57:00 -0300

    feat - new_user route

--- client/urls.py
+++ client/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login', client_views.home_login, name="login"),
     path('logout', client_views.user_logout, name="logout"),
     path('', client_views.home, name="home"),
-    path('user', client_views.home_user, name="user"),
+    path('user', client_views.home_user, name="home_user"),
+    path('user/new', client_views.new_user, name="new_user"),
 ]
